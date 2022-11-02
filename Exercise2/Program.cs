f7bcf6a14ddc32a4077fa23d3e625565c296eff4
FadliStev <wiranto.akbar.ft21@mail.umy.ac.id>
2022-11-02 15:36:54 +0700

    add method and function for element of array

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -11,9 +11,18 @@ namespace Exercise2
         int[] fad = new int[24];//create new array
         int n;//add variable
 
-        public void read()
+        public void read()//menambahkan method untuk element array
         {
-
+            while (true)//menambahkan function  
+            {
+                Console.WriteLine("Masukkan banyaknya element/data pada array : ");
+                string s = Console.ReadLine();  
+                n = Int32.Parse(s);
+                if (n <= 20)//menambahkan function
+                    break;
+                else
+                    Console.WriteLine("\n Element yang diamsukkan tidak bisa lebih dari 24");
+            }
         }
         static void Main(string[] args)
         {
